a667be0ec8d619552abed5bed6ec253e849007c0
cgar246 <christiang3268@yahoo.com>
2021-09-03 07:55:18 -0700

    added coloring, rows, and the save button. Save button doesnt save will work on that next

--- Develop/JavaScript.js
+++ Develop/JavaScript.js
@@ -4,11 +4,11 @@ var workLoad = [
     {time: "1000", task:""},
     {time: "1100", task:""},
     {time: "1200", task:""},
-    {time: "0100", task:""},
-    {time: "0200", task:""},
-    {time: "0300", task:""},
-    {time: "0400", task:""},
-    {time: "0500", task:""},
+    {time: "1300", task:""},
+    {time: "1400", task:""},
+    {time: "1500", task:""},
+    {time: "1600", task:""},
+    {time: "1700", task:""},
 ];
 
 // Will set the date
@@ -38,7 +38,13 @@ $(".savetask").on("click", function (event) {
 // Creating tasks
 workLoad.forEach(function(block, index){
     var hourincrement = block.time;
-    var sectionColour = timecolor(hourincrement)
+    var sectionColour = timecolor(hourincrement);
+    var sections = 
+        '<div class ="task-info row" id="' + index + '"><div class="col-1 hour pt-4">' 
+        + hourincrement + '</div><div class="col task"><textarea class="form-control ' 
+        + sectionColour + '">' + block.task + '</textarea></div><button class="col-1 saveBtn d-flex justify-content-center align-items-center"><i class="mt-2 far fa-save fa-lg"></i></button></div>'
+
+    $(".container").append(sections);
 })
 
 // Saves tasks to localstorage
@@ -47,7 +53,18 @@ var saved = function () {
 }
 
 // Colorful row changes
-
+function timecolor(time) {
+    var planNow = moment(horita, "H A");
+    var planEntry = moment(time, "H A");
+    console.log(planEntry);
+    if (planNow.isBefore(planEntry) === true) {
+        return "future";
+    } else if (planNow.isAfter(planEntry) === true) {
+        return "past";
+    } else {
+        return "present";
+    }
+}
 // Load tasks to localStorage
 
 var loading = JSON.parse (localStorage.getItem("tasks"))
